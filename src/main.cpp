b1ed5707f141fd8cb1cf65aa9d81c277b5cf6b32
maksimdodep <stevenubloo@gmail.com>
2026-05-17 19:27:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,43 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
-			return false;
-		}
-
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
-
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
-
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
+class $modify(PlayLayer) {
+    void updateProgressLabel() {
+        // 1. Сначала запускаем оригинальный код игры, чтобы обновился текущий процент
+        PlayLayer::updateProgressLabel();
+        
+        // 2. Проверяем, существует ли текстовое поле процентов на экране
+        if (m_progressLabel) {
+            
+            // Если игрок заспавнился со StartPos (в режиме теста или практики с точки)
+            if (m_isTestMode || m_isPracticeMode) {
+                double startPercent = 0.0;
+                
+                // Достаем настройки уровня и проверяем физический объект StartPos
+                if (m_levelSettings && m_levelSettings->m_startPosition) {
+                    float startX = m_levelSettings->m_startPosition->getPosition().x;
+                    
+                    // Защита от деления на ноль (проверяем длину уровня)
+                    if (m_levelLength > 0.0f) {
+                        startPercent = (startX / m_levelLength) * 100.0;
+                        
+                        // Ограничиваем рамками от 0 до 100 на всякий случай
+                        if (startPercent < 0.0) startPercent = 0.0;
+                        if (startPercent > 100.0) startPercent = 100.0;
+                    }
+                }
+                
+                // Получаем текущий процент игрока
+                int currentPercent = this->getCurrentPercent();
+                
+                // Форматируем строку в нужный вид: [Старт]% - [Текущий]%
+                std::string formatStr = fmt::format("{:.0f}% - {}%", startPercent, currentPercent);
+                
+                // Силой заменяем стандартный текст игры на наш кастомный сверху экрана
+                m_progressLabel->setString(formatStr.c_str());
+            }
+        }
+    }
 };
