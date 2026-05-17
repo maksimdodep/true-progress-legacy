2a8eb929aa93a9134885f1adb3fde94227af8fc4
maksimdodep <stevenubloo@gmail.com>
2026-05-17 19:32:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,39 +4,39 @@
 using namespace geode::prelude;
 
 class $modify(PlayLayer) {
-    void updateProgressLabel() {
-        // 1. Сначала запускаем оригинальный код игры, чтобы обновился текущий процент
-        PlayLayer::updateProgressLabel();
-        
-        // 2. Проверяем, существует ли текстовое поле процентов на экране
-        if (m_progressLabel) {
+    // В старых версиях 2.2074 хукаем именно этот метод обновления процентов
+    void updatePercentLabel() {
+        // 1. Сначала даем игре обновиться в штатном режиме
+        PlayLayer::updatePercentLabel();
+
+        // 2. Проверяем, существует ли интерфейс UI и сам счетчик процентов на экране
+        if (m_uiLayer && m_uiLayer->m_percentageLabel) {
             
-            // Если игрок заспавнился со StartPos (в режиме теста или практики с точки)
+            // Если игрок запустил уровень со StartPos
             if (m_isTestMode || m_isPracticeMode) {
-                double startPercent = 0.0;
                 
-                // Достаем настройки уровня и проверяем физический объект StartPos
-                if (m_levelSettings && m_levelSettings->m_startPosition) {
-                    float startX = m_levelSettings->m_startPosition->getPosition().x;
-                    
-                    // Защита от деления на ноль (проверяем длину уровня)
+                // В старых версиях игры стартовый процент рассчитывается встроенной функцией
+                float startPercent = 0.0f;
+                if (m_levelSettings) {
+                    // Используем безопасный дефолтный расчет или старую переменную позиции
+                    startPercent = m_levelSettings->m_startPos.x; 
                     if (m_levelLength > 0.0f) {
-                        startPercent = (startX / m_levelLength) * 100.0;
-                        
-                        // Ограничиваем рамками от 0 до 100 на всякий случай
-                        if (startPercent < 0.0) startPercent = 0.0;
-                        if (startPercent > 100.0) startPercent = 100.0;
+                        startPercent = (startPercent / m_levelLength) * 100.0f;
                     }
                 }
                 
+                // Ограничиваем рамками
+                if (startPercent < 0.0f) startPercent = 0.0f;
+                if (startPercent > 100.0f) startPercent = 100.0f;
+
                 // Получаем текущий процент игрока
                 int currentPercent = this->getCurrentPercent();
-                
+
                 // Форматируем строку в нужный вид: [Старт]% - [Текущий]%
                 std::string formatStr = fmt::format("{:.0f}% - {}%", startPercent, currentPercent);
-                
-                // Силой заменяем стандартный текст игры на наш кастомный сверху экрана
-                m_progressLabel->setString(formatStr.c_str());
+
+                // Заменяем текст старого счетчика в UI слое игры
+                m_uiLayer->m_percentageLabel->setString(formatStr.c_str());
             }
         }
     }
